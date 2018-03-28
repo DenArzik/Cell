9448adef71a54b60b2ee4a6b283234df25b4963b
DenArzik <denisthekwondist@gmail.com>
2018-03-28 19:47:27 +0300

    Implemented basic Movement Component (gonna be changed)

---
+++ Source/Cell/Pawns/PawnMovement.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "PawnMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CELL_API UPawnMovement : public UPawnMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
